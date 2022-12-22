68d667d7e135f79d513a1b84d16770f26a8bddfa
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-12-22 18:44:20 +0800

    Add files via upload

--- downloader_pausable_LY.py
+++ downloader_pausable_LY.py
@@ -3,26 +3,26 @@ import os, sys, argparse, re
 import requests
 import urllib.parse
 
-
+config_file="config.ly"
 temp_file="temp.ly"
+log_file="../log.txt"
 pause_file="../pause.ly"
 download_dir="download"
-lock_suffix=".lock.ly"
+
+just_resume=1
 
 class Temp_data:
 	def __init__(self, f_temp_arr):
-		for i in range(1, 6):
+		for i in range(0, 2):
 			f_temp_arr[i] = int(f_temp_arr[i])
-		self.url_template, \
-		self.min_int, \
-		self.max_int, \
-		self.is_numbering, \
 		self.current_strg_counter, \
-		self.current_counter \
+		self.current_counter, \
+		self.lock_name \
 		 = f_temp_arr
 
 	def __str__(self):
 		return str(self.__dict__)
+
 
 	def update_temp(self, f_temp):
 		f_temp.seek(0)
@@ -33,6 +33,20 @@ class Temp_data:
 		#force flushing
 		f_temp.flush()
 		os.fsync(f_temp.fileno())
+
+class Config_data:
+	def __init__(self, f_config_arr):
+		for i in range(1, 5):
+			f_config_arr[i] = int(f_config_arr[i])
+		self.url_template, \
+		self.min_int, \
+		self.max_int, \
+		self.is_numbering, \
+		self.is_logging, \
+		 = f_config_arr
+
+	def __str__(self):
+		return str(self.__dict__)
 
 #def convert_int(arr, start, end):
 #	return_arr = arr
@@ -64,6 +78,19 @@ def extract_headers(header_file):
 				headers.update({headers_split[0]: headers_split[1]})
 		return headers
 	return None
+	
+def valid_filename(f_temp, temp_data, filename):
+	f = os.path.basename(filename)
+	name, extension = os.path.splitext(f)		#diff with bash e.g. .bashrc
+	count = 1
+	while os.path.exists(f) and f != temp_data.lock_name:
+		f = name+"."+str(count)+extension
+		count += 1
+
+	temp_data.lock_name = f
+	temp_data.update_temp(f_temp)
+
+	return f
 
 def grep_content_disposition(dispos_strg):
 	try:
@@ -73,7 +100,7 @@ def grep_content_disposition(dispos_strg):
 			name = name[name.rindex("'")+1:]
 		else:
 			name = re.search('filename[^=*]*=[^;]*', dispos_strg, re.IGNORECASE)
-			print(name)
+			#print(name)
 			if name is not None:
 				name = name.group()
 				name = name.split('"')
@@ -83,39 +110,86 @@ def grep_content_disposition(dispos_strg):
 		print('grep_content_disposition exception, response headers')
 		return None
 
-def dl_resource(f_temp, temp_data, line, headers):
-	for temp_data.current_counter in range(temp_data.current_counter, temp_data.max_int+1):
-		temp_data.update_temp(f_temp)
-		try:
-			os.remove(filename)
-		except OSError:
-			pass
+def printf_log(f_log, strg, is_logging):	#f_log must be opened before call
+	print(strg, end='')
+	if is_logging:
+		#force flushing
+		f_log.write(strg)
+		f_log.flush()
+		os.fsync(f_log.fileno())
 
-		url = temp_data.url_template.replace(';s', line).replace(';d', str(temp_data.current_counter))
-
-		r = requests.get(url, allow_redirects=True, headers=headers)
-		if 'content-disposition' in r.headers:		#r.headers is caseinsensitive-type dict
-			fname = grep_content_disposition(r.headers['content-disposition'])
-			print(str(fname))
+def dl_resource(f_temp, f_log, config_data, temp_data, line, headers):
+	#equivalent:  "$cookie_file" != "" => headers != None
+	#			  "$is_logging"	 != "" => f_log != None
+	global just_resume
+	for temp_data.current_counter in range(temp_data.current_counter, config_data.max_int+1):
+		if just_resume:
+			just_resume = 0
 		else:
-			fname = urllib.parse.unquote(r.url[r.url.rindex('/')+1:])
+			temp_data.lock_name = ""
+			temp_data.update_temp(f_temp)
+		if os.path.exists(pause_file):
+			exit
 
-		print(fname, end='')
-		try:
-			byte = open(fname, 'wb').write(r.content)
-			if not byte > 0:
-				print('[E]')
+		url = config_data.url_template.replace(';s', line).replace(';d', str(temp_data.current_counter))
+		#print(url)
+
+		#if args.cookie_file is not None:
+		#get header merged with downlod here
+		if headers is not None:			#same as above
+			r = requests.get(url, allow_redirects=True, headers=headers)
+		else:
+			r = requests.get(url, allow_redirects=True)
+
+		#print(r.headers)
+
+		filename = None
+		if not config_data.is_numbering:
+			if 'content-disposition' in r.headers:		#r.headers is caseinsensitive-type dict
+				filename = grep_content_disposition(r.headers['content-disposition'])
 			else:
-				print('[S]')
+				filename = urllib.parse.unquote(os.path.basename(r.url))
+			filename = valid_filename(f_temp, temp_data, filename)
+
+		if filename is None:
+			if line == "":
+				filename = str(temp_data.current_counter)
+			else:
+				purged_line = urllib.parse.unquote(os.path.basename(line))
+				if config_data.max_int == '0':
+					filename = purged_line
+				else:
+					filename = str(temp_data.current_counter)+'-'+purged_line
+
+		printf_log(f_log, "\n"+filename, config_data.is_logging)
+
+		try:
+			byte = open(filename, 'wb').write(r.content)
+			if byte > 0:
+				printf_log(f_log, '[S]', config_data.is_logging)
+			else:
+				printf_log(f_log, '[E]', config_data.is_logging)
 		except:
-			print('[E] wf')		#write file err
+			printf_log(f_log, '[E] wf', config_data.is_logging)	#write file err
 
-
-#		remotefile = urlopen(url, cookies=cookies)
-#		blah = remotefile.info()['Content-Disposition']
-#		value, params = cgi.parse_header(blah)
-#		filename = params["filename"]
-#		urlretrieve(url, filename)
+#		r = requests.get(url, allow_redirects=True, headers=headers)
+#		if 'content-disposition' in r.headers:		#r.headers is caseinsensitive-type dict
+#			fname = grep_content_disposition(r.headers['content-disposition'])
+#			print(str(fname))
+#		else:
+#			fname = urllib.parse.unquote(os.path.basename(r.url))
+#
+#		print(fname, end='')
+#		try:
+#			byte = open(fname, 'wb').write(r.content)
+#			if not byte > 0:
+#				print('[E]')
+#			else:
+#				print('[S]')
+#		except:
+#			print('[E] wf')		#write file err
+#
+#		temp_data.update_temp(f_temp)
 
 
 parser = argparse.ArgumentParser(description='Optional app description')
@@ -128,56 +202,85 @@ parser.add_argument('-max', '--max_int',
                     help='counter end int')
 parser.add_argument('-num', '--is_numbering',
                     help='use name from counter / strg file')
+parser.add_argument('-log', '--is_logging',
+                    help='write log')
 parser.add_argument('-strg', '--strg_file',
                     help='string file')
 parser.add_argument('-cookie', '--cookie_file',
                     help='cookie file')
 args = parser.parse_args()
+
 if args.strg_file is not None:
 	args.strg_file = os.path.realpath(args.strg_file)
 if args.cookie_file is not None:
 	args.cookie_file = os.path.realpath(args.cookie_file)
 
 if ";d" in args.url_template:
-	args.max_int = '0' if args.max_int is None else args.max_int
+	args.max_int = str(2**63-1) if args.max_int is None else args.max_int
 	args.min_int = '0' if args.min_int is None else args.min_int
 else:
 	args.max_int = '0'
 	args.min_int = '0'
 args.is_numbering = args.is_numbering or '1'
+args.is_logging = args.is_logging or '1'
 
-if not os.path.exists(temp_file):
-	with open(temp_file, "w") as f_temp:
-		f_temp.write('\n'.join([args.url_template,
+if not os.path.exists(config_file):
+	with open(config_file, "w") as f_config:
+		f_config.write('\n'.join([args.url_template,
 								args.min_int,
 								args.max_int,
 								args.is_numbering,
-								'1',
-								args.min_int,
+								args.is_logging,
 								'']))
+if not os.path.exists(temp_file):
+	with open(temp_file, "w") as f_temp:
+			f_temp.write('\n'.join(['1',
+									args.min_int,
+									'',
+									'']))
+
+#read
+with open(config_file, "r") as f_config:
+	config_data = Config_data(f_config.read().splitlines())
+	#print(str(config_data))	#testing
 
 with open(temp_file, "r+") as f_temp:
 	temp_data = Temp_data(f_temp.read().splitlines())
+	#print(str(temp_data))		#testing
+#end read
 
 	if not os.path.exists(download_dir):
 	   os.makedirs(download_dir)
 	os.chdir(download_dir)
 	temp_file = "../"+temp_file
 
+	#log
+	if config_data.is_logging is not None:
+		f_log = open(log_file, "a")
+	else:
+		f_log = None
+	#end log
+
 	#cookie
 	#cookies = extract_cookie(args.cookie_file)
 	headers = extract_headers(args.cookie_file)
-	#end of cookie
-			
+	#end cookie
+
 	if args.strg_file is None:
-		dl_resource(f_temp, temp_data, "", cookies)
+		dl_resource(f_temp, f_log, config_data, temp_data, "", headers)
 	else:
 		with open(args.strg_file, "r") as f_strg:
 
 			for j, line in enumerate(f_strg):
 				if j+1 >= temp_data.current_strg_counter:
-					#print(j, line, end='')
+					#print("this_line: ", j, line, end='')
 					line = line.rstrip('\r\n')
 					temp_data.current_strg_counter = j+1
-					dl_resource(f_temp, temp_data, line, headers)
-					temp_data.current_counter = temp_data.min_int
+					dl_resource(f_temp, f_log, config_data, temp_data, line, headers)
+					#just_resume = 0	#bash func cannot visit global var
+					temp_data.current_counter = config_data.min_int
+
+	#close log
+	if config_data.is_logging is not None:
+		f_log.close()
+	#end close log
